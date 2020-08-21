550acc673b51d1c744c3142808903fc38f994cf7
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-08-21 17:47:50 +0500

    Fixed indententations and spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   mount_uploader :profile_photo, AvatarUploader
 
-  validates_presence_of   :profile_photo
-  validates_integrity_of  :profile_photo
+  validates_presence_of :profile_photo
+  validates_integrity_of :profile_photo
   validates_processing_of :profile_photo
 
 end
