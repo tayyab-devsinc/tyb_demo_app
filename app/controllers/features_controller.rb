1f0d9258a300063991dc35755a2e9d3f96e06d9e
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-08-21 12:42:18 +0500

    Changed User table admin type from string to boolean

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,7 +1,15 @@
 class FeaturesController < ApplicationController
     
+    before_action :authenticate_user!
+    before_action :admin_user
+    
     def new
         @feature = Feature.new
     end
+
+
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
+      end
 
 end
