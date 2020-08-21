9cda9a3b2438559b989b2527d484f5ade2c2d4f0
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-08-21 16:58:21 +0500

    Added index for Features

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,35 +1,35 @@
 class FeaturesController < ApplicationController
-    
-    before_action :authenticate_user!
-    before_action :admin_user
-    
-    def index
-        @features = Feature.all
-      end
-    
-    def new
-        @feature = Feature.new
+
+  before_action :authenticate_user!
+  before_action :admin_user, except: [:index]
+
+  def index
+    @features = Feature.all
+  end
+
+  def new
+    @feature = Feature.new
+  end
+
+  def create
+    @feature = Feature.new(feature_params)
+    if @feature.save
+      flash[:success] = 'Feature Successfully Added'
+      redirect_to features_url
+    else
+      flash[:danger] = 'Error occurred, Try Again'
+      render 'new'
     end
+  end
 
-    def create
-        @feature = Feature.new(feature_params)
-        if @feature.save
-          flash[:success] = "Feature Successfully Added"
-          redirect_to features_url
-        else
-            flash[:danger] = "Error occured, Try Again"
-            render 'new'
-        end
-      end
-
-    def admin_user
-        redirect_to(root_url) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
 
-    private
-  
-      def feature_params
-        params.require(:feature).permit(:name, :code, :unit_price, :max_unit_limit)
-      end
+  private
+
+  def feature_params
+    params.require(:feature).permit(:name, :code, :unit_price, :max_unit_limit)
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
-end
 
-private
-def sign_up_params 
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  private
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-def account_update_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
