0a68920686548766c351b6ebf7af0788c464b156
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-01 12:00:13 +0500

    Transaction blocked moved

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -30,23 +30,12 @@ class SubscriptionsController < ApplicationController
 
   def subscribe
     ActiveRecord::Base.transaction do
-      subscription = Subscription.new
-      subscription.plan_id = params[:subscription_id]
-      subscription.user_id = current_user.id
-      subscription.subscription_date = Date.today
-      subscription.billing_day = Date.today.day > 28 ? 28 : Date.today.day
+      subscription = Subscription.new(plan_id: params[:subscription_id],
+                                      user_id: current_user.id,
+                                      subscription_date: Date.today,
+                                      billing_day: Date.today.day > 28 ? 28 : Date.today.day)
       if subscription.save
-        t = Transaction.new
-        t.subscription_id = subscription.id
-        t.user_id = subscription.user_id
-        t.amount = subscription.plan.monthly_fee
-        if t.save
-          flash[:success] = 'Subscribed Successfully'
-          redirect_to plans_url
-        else
-          flash[:danger] = 'Error occurred, Try Again'
-          render plans_url
-        end
+
       else
         flash[:danger] = 'Error occurred, Try Again'
         render plans_url
