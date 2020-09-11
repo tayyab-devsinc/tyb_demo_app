2f98e7c1b3d924463aab3868eca4c43b4ab8bf23
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-11 19:36:50 +0500

    Removed BIN files

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -2,10 +2,7 @@ class Usage < ApplicationRecord
   belongs_to :subscription
   belongs_to :feature
 
-  accepts_nested_attributes_for :feature
-  accepts_nested_attributes_for :subscription
-
-  validates :feature_count, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :feature_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def self.calculate_fee(subscription)
     fee = subscription.plan.monthly_fee
