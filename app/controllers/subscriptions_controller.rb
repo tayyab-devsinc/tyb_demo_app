8bb87c9f455f945edf001048f67fc202ee604f36
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-15 17:06:09 +0500

    Removed exrta code from Subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -39,29 +39,6 @@ class SubscriptionsController < ApplicationController
     redirect_to usages_url
   end
 
-  def update_usage
-    print("\n\n PLEASE UPDATE ME : #{usage_params}")
-    # if @subscription.usages.create(usage_params)
-    #   flash[:success] = 'Usage Updated'
-    # else
-    #   flash[:danger] = 'Error occurred, Try Again'
-    # end
-    # redirect_to usages_url
-  end
-
-  def all_usages
-    @subscriptions = if current_user.admin?
-                       Subscription.includes(:plan).paginate(page: params[:page], per_page: 10)
-                     else
-                       current_user.subscriptions.includes(:plan).paginate(page: params[:page], per_page: 10)
-                     end
-    print("\n\n HIHIHI\n\n")
-  end
-
-  def edit_subscription_usage
-    print("\n\n HIHIHI\n\n")
-  end
-
   private
 
   def usage_params
@@ -79,5 +56,4 @@ class SubscriptionsController < ApplicationController
   def set_usage
     @usage = Usage.first
   end
-
 end
