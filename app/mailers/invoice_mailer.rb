3a25f088956e3089dc9557680b9415065e285ae8
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-15 17:13:44 +0500

    Fixed emails and urls of example.com

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,10 +1,10 @@
 class InvoiceMailer < ApplicationMailer
-  default from: 'tyb_demo_app@example.com'
+  default from: 'muhammadt.tayyab@devsinc.com'
 
   def invoice_email(current_user_id, transaction_id)
     @user = User.find_by_id(current_user_id)
     @transaction = Transaction.find_by_id(transaction_id)
-    @url = 'http://example.com/login'
+    @url = 'http://tyb_demo_app.herokuapp.com'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
