e23dc66f7bcf7f69c89ddfb1ae907336b65ec9e5
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-17 17:29:00 +0500

    * removed daily_transaction method from Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :subscription
   belongs_to :user
-
-  def self.daily_transaction
-    subs = Subscription.all.where(billing_day: Date.today.day)
-    subs.each(&:charge_fee)
-  end
 end
