f38fb63dd4cddbf6fcaf3edcc169f2105960ba43
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-09-21 19:25:24 +0500

    * model counts added on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :set_counts, only: [:home]
+  before_action :authenticate_user!, only: [:home]
   def home
   end
 
@@ -9,5 +11,14 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+  end
+
+  private
+
+  def set_counts
+    @counts = { feature_count: Feature.count,
+                plan_count: Plan.count,
+                subscription_count: Subscription.count,
+                user_count: User.count }
   end
 end
